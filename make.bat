46a3522c6fa00bd991956a54d0b8f8276bd79728
FredThx <fredthx@gmail.com>
2019-04-13 16:52:27 +0200

    properties for windows

--- make.bat
+++ make.bat
@@ -1,11 +1,13 @@
 @echo off
+python make_properties.py
 pyinstaller ^
   --onefile ^
   --clean ^
   --noupx ^
   --win-private-assemblies ^
-  --icon .\fcalc.ico ^
+  --icon=.\fcalc.ico ^
   --noconfirm ^
   --noconsole ^
+  --version-file=properties.rc ^
   fcalc.py
 pause
