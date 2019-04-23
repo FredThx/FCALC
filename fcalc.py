e8d19c3d31c491bdf390a093f4049f5cc1d9d199
Fred Thx <FredThx@users.noreply.github.com>
2019-04-23 23:26:51 +0200

    logfile_level = INFO

--- fcalc.py
+++ fcalc.py
@@ -3,7 +3,7 @@
 import FCALC
 from FUTIL.my_logging import *
 
-my_logging(console_level = DEBUG, logfile_level = DEBUG, details = True)
+my_logging(console_level = DEBUG, logfile_level = INFO, details = True)
 
 App = FCALC.Fcalc(url_help = 'https://github.com/FredThx/FCALC/wiki/Help')
 App.run()
